e4c10124e168c0801303aa2be03ea746a09b6d18
thiedo <dothiejr@gmail.com>
2025-05-31 01:00:27 +0000

    middle update data type file

--- data_type.py
+++ data_type.py
@@ -1,6 +1,29 @@
-#  int, float, string, list and set
+#DATA TYPE TESTING FILE
+# a python variable can be assigns multiple data type
+"""
 x = 50
 x = 60.5
-x = "Hello World"
+print(x)
+print(type(x))
+x = "Hello World
 x = ["geeks", "for", "geeks"]
 x = ("geeks", "for", "geeks")
+
+printing variables type
+a = 5 
+print("type a = ",type(a))
+b = 5.
+print("type b = ",type(b))
+c = 2 + 4j
+print("type c = ",type(c))
+
+#String data type
+s = 'Welcome to the Geeks World'
+print(s)
+print(type(s))
+--Access String index
+print(s[0])
+print(s[1])
+print(s[9])
+print(s[-1])
+"""
