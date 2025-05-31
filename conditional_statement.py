1a7edaa221d5b9f176d04cb9ab729b023acad4fa
thiedo <dothiejr@gmail.com>
2025-05-31 16:44:34 +0000

    added loop file and finalize conditional

--- conditional_statement.py
+++ conditional_statement.py
@@ -1,6 +1,35 @@
 #CONDITIONAL STATEMET TESTING FILE
 #-1 if statement
-age = 20
+"""age = 20
 
 if age >= 18:
     print("Eligible to vote.")
+#testing Else condition
+age = 11
+if age <= 12:
+    print("Travel for free.")
+else:
+    print("Pay for ticket.")
+marks = 45
+res = 'pass' if marks >= 40 else 'fail'
+print(f"result:{res}")
+#testing elif statement
+age = 25
+if age <= 12:
+    print("Child.")
+elif age <= 19:
+    print("Teenager.")
+elif age <= 35:
+    print("Young adult.")
+else:
+    print("Adult.")
+
+number = 2
+#match-case statement 
+match number:
+    case 1:
+        print("One")
+    case 2 | 3:
+        print("Two or Three")
+    case _:
+        print("Other number")"""
--- data_type.py
+++ data_type.py
@@ -26,4 +26,53 @@ print(s[0])
 print(s[1])
 print(s[9])
 print(s[-1])
-"""
+
+#List data type
+a = []
+a = ['pauline', 24, "laure", 60, 'Mamy', 31]
+print("Accessing element from the list")
+print('a[0  ]=',a[0])
+print(a[1])
+print(a[2])
+print(a[-1])
+print(a[-2])
+print(a)
+print(type(a))
+
+#tuple data type 
+tup = () #E;pty tuple initiating but it need at least one comma to be a fonctional tuple
+tup2 = ('Daniel', 'Louis', 'Beatrice', 'Cira')
+print(tup2)
+print(type(tup))
+print(type(tup2))
+
+#bool datas has value True/False
+#tuple managment
+s1 = set()
+s1 = set("GeeksforGeeks")
+print("Set with the use of String: ", s1)
+print(s1)
+s2 = set(["Geeks", "For", "Geeks"])
+#print("Set with the use of List: ", s2)
+for l in s2:
+    print(l, end=" ")
+# check if item exist in set    
+print("Oumar" in s2)
+
+#Dictionnaries
+d = {}
+d = {1:'thiedo', 2:'mamy', 3:'baba', 'Dany':'Marie'}
+print(d['Dany'])
+nom = d.get(1)
+print(nom) """
+#EXERCISE
+fruits = ["apple", "banana", "orange"]
+print(fruits)
+fruits.append("grape")
+print(fruits)
+fruits.remove("orange")
+print(fruits)
+fruits.append("pinepale")
+print(fruits)
+fruits.remove("pinepale")
+print(fruits)
