62acde56b0d7a9290c90edc35b87e018107531a9
thiedo <dothiejr@gmail.com>
2025-06-02 17:51:01 +0000

    final edit loop

--- loops.py
+++ loops.py
@@ -1,1 +1,37 @@
-#while loop 
+"""while loop 
+count = 0
+while count < 3:
+    count = count +1 
+    print("hello Geek ",count)
+else: #with else loop
+    print("In Else Block")
+#infinite while loop
+cnt = 0
+while cnt == 0:
+    print("hello Geek Welcome to our infinite loop") """
+#Data type iteration using for loop
+"""li = ["geeks", "for", "geeks"]
+for i in li:
+    print(i)
+
+tup = ("geeks", "for", "geeks")
+for i in tup:
+    print(i)
+  
+s = "Geeks"
+for i in s:
+    print(i)
+    
+d = dict({'x':123, 'y':354})
+for i in d:
+    print("%s  %d" % (i, d[i]))
+    
+set1 = {1, 2, 3, 4, 5, 6}
+for i in set1:
+    print(i)
+   
+#terating by the Index of Sequences
+list = ["geeks", "for", "geeks"]
+for index in range(len(list)):
+    print(index,"=",list[index])
+"""
