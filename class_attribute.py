f9060d0ce24d9983388a9241f5105ecd751c89de
thiedo <dothiejr@gmail.com>
2025-06-03 20:45:27 +0000

    validate class file

--- class_attribute.py
+++ class_attribute.py
@@ -11,7 +11,7 @@ car1 = Car("Toyota", "Corolla")
 
 # Call the display_info method
 print(car1.display())  
-"""
+
 class gfg:
     def __init__(self, topic):
         self._topic = topic  
@@ -23,3 +23,41 @@ class gfg:
 ins = gfg("Python")
 
 ins.topic()
+---------
+def msg(name):
+    return f"hello, {name}!"
+f = msg
+print(f("Anurag"))
+
+def fun1(fun2, nom):
+    return fun2(nom)
+print(fun1(msg,"Bob"))
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+# Storing functions in a dictionary
+d = {
+    "add": add,
+    "subtract": subtract
+}
+
+# Calling functions from the dictionary
+print(d["add"](5, 3))       
+print(d["subtract"](5, 3))
+
+#LAMBDA EXPRESSION 
+# Example: Check if a number is positive, negative, or zero
+n = lambda x: "Positive" if x > 0 else "Negative" if x < 0 else "Zero"
+
+print(n(5))   
+print(n(-3))  
+print(n(0))
+"""
+# Example: Filter even numbers from a list
+n = [1, 2, 3, 4, 5, 6]
+even = filter(lambda x: x % 2 == 0, n)
+print(list(even))
