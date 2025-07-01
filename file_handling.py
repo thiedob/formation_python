7bb01f449239d47da960f4365b600105e726a7ef
thiedo <dothiejr@gmail.com>
2025-07-01 06:07:58 +0000

    add file_handling, tuto.txt file and valid_choix function in test2 file

---
+++ file_handling.py
@@ -0,0 +1,26 @@
+# Open the file and read its contents
+#with open('geeks.txt', 'r') as file:
+
+#reading a file in normal mode
+"""
+file = open('tuto.txt','r')   
+content = file.read()
+print(content)
+file.close()
+
+#reading a file in binary mode
+file = open("tuto.txt", "rb")
+contenue = file.read()
+print(contenue)
+file.close()
+
+#writing to a file
+file = open("tuto.txt", "w")
+file.write("ceci est le nouveau texte ecrit depuis le language python")
+file.close()
+
+"""
+file = open("tuto.txt", "rb")
+contenue = file.read()
+print(contenue)
+file.close()
--- test2.py
+++ test2.py
@@ -3,31 +3,85 @@ Menu  all menu request #155# and #100# (entering menu #155# et #100#)
 if wrong number reject 
 """
 import sys
+import os
+
+
+def valid_choix(chaine = ['1','2','3','4','5','6','7','8','9']):
+    cpt = 0
+    car = len(chaine)
+    test = 0
+    while test == 0 and cpt <3:
+        try:
+            choix = int(input("Veuillez entrer votre choix "))
+            for i in  chaine:
+                trouve = True if i == str(choix) else False
+                if trouve :
+                    test = 1
+                    break
+            if not trouve:
+                test = 0
+                cpt += 1
+                print("Veuillez choisir un entier parmi les choix ci-dessous")
+                print(chaine)
+                #pass
+        except:
+            #pass
+            print("Veuillez choisir un entier parmi les choix ci-dessous")
+            print(chaine)
+            test = 0
+            cpt += 1
+    if cpt == 3:
+        os.system('clear')
+        try:
+            sys.exit()
+        except SystemExit as e:
+            print("Vous avez atteint ",cpt," tentatives merci de revenir une prochaine fois")   
+    else:
+        os.system('clear')
+        return choix
+    #pass
 
 def valider_choix(chaine = "123456789"):
-    #pass
-    car = chaine[-1]
-    test1 = 0
-    while test1 == 0:
+    cpt = 0
+    debut = chaine[0]
+    fin = chaine[-1]
+    test = 0
+    while test == 0 and cpt <3:
         try:
             choix = int(input("Veuillez entrer votre choix "))
             trouve = False if str(choix) not in chaine else True
-            if trouve:
-                test1 = 1
-                print("CHOIX CORRECT")
-                print(choix)
+            if trouve :
+                test = 1
+                #print("CHOIX CORRECT")
             else:
-                print("Vous devez saisir un chiffre entre 1 et ",car)               
-                test1 = 0    
+                test = 0
+                cpt +=1
+                if chaine == "1":
+                    print("1 est le seul choix possible merci de Rectifier")
+                else:    
+                    print("Vous devez saisir un chiffre entre ",debut," et ",fin)     
+                continue            
         except:
             print("Veuillez saisir un nombre entier")
-    return choix
+            test = 0
+            cpt += 1
+    if cpt == 3:
+        os.system('clear')
+        try:
+            #print(cpt)
+            sys.exit()
+        except SystemExit as e:
+            print("Vous avez atteint ",cpt," tentatives merci de revenir une prochaine fois")   
+    else:
+        os.system('clear')
+        return choix
+
 
 def saisir_code():
     #pass
     test = 0
-    cpt = 0
-    while test == 0 and cpt <3:
+    cpt1 = 0
+    while test == 0 and cpt1 <3:
         saisi = input("Veuillez entrez le code souhaite: ")
 
         match saisi:
@@ -37,7 +91,7 @@ def saisir_code():
             case "#155#":
                 #print("Vous avez choisi l'option #155#")
                 print("Portail YAS")
-                print("1:3000F = 24Go+300mn+100SMS")
+                print("1:Bons plans")
                 print("3:Illimax")
                 print("7:Internet")
                 print("2:Voix")
@@ -46,20 +100,70 @@ def saisir_code():
                 print("6:Infos Utiles")
                 print("8:Changement Nom Reseau")
                 print("9:NOUVEAU:MYCANAL")
+                #os.system('clear')
                 choice = valider_choix()
                 test = 1
                 match choice:
                     case 1:
-                        pass
+                        #pass
                         print("Mes offres")
                         print("1. 1000F = 5Go+30j")
                         print("2. 2000F = 15Go+250mn-30j")
                         print("3. 3000F = 24Go+300mn+100SMS ")
-                        print("4. 2500F = 6Go+160mn_30j")
-                        choice1 = valider_choix("1234")
+
+                        choice1 = valider_choix("123")
+                        if choice1 == 1:
+                            print("Vous allez activer le forfait 5Go a 1000F valide 30J")
+                            print("1:Confirmer")
+                            choice2 = valider_choix("1")
+                            if choice2 == 1:
+                                print("Votre demande est en cours de traitement.Un sms vous sera envoye")
+                        elif choice1 == 2:
+                            print("Vous allez activer le forfait Illimax 2000F: 15Go+250mn+100SMSts reseaux valable 30J")
+                            print("1:Confirmer")
+                            choice2 = valider_choix("1")
+                            if choice2 == 1:
+                                print("Votre demande est en cours de traitement.Un sms vous sera envoye")
+                        else:
+                            print("Vous allez acheter la promo ILLIMAX 3000F valable 30j: 300mn+24Go+100SMS")
+                            print("1:Confirmer")
+                            choice2 = valider_choix("1")
+                            if choice2 == 1:
+                                print("Votre demande est en cours de traitement.Un sms vous sera envoye")
                     case 2:
-                        pass
+                        #pass
+                        print("Offres Voix")
+                        print("1:Tawfekh")
+                        print("2:Samawaye")
+                        print("0:Accueil ")
                         choice1 = valider_choix("012")
+                        match choice1:
+                            case 1:
+                                print("Tawfekh(Tous reseaux)")
+                                print("5:200F: 50Min-24h")
+                                print("1:500F: 130Min-24h")
+                                print("2:1000F: 400 Min-7j")
+                                print("3:2000F: 750 Min-30j")
+                                print("4:5000F: 1650 Min-30j")
+                                print("0: Accueil")
+                                #pass
+                                choice2 = valider_choix("012345")
+                                if choice2 == 1:
+                                    print("130min ts reseaux valable 24h a 500F")
+                                    print("1:Confirmer")
+                                    print("2:Acheter pour un autre")
+                                    print("99.Precedent")
+                                    print("0.Accueil") 
+                                    choice3 = valid_choix(['1','2','99','0']) 
+                                    if choice3 == 1:
+                                        #pass 
+                                        print("Votre requete est en cours de traitement")
+                                elif choice2 == 2:
+                                    pass
+                            case 2:
+                                pass
+                            case _:
+                                pass
                     case 3:
                         pass
                         choice1 = valider_choix("012")
@@ -82,14 +186,24 @@ def saisir_code():
                         pass
                         choice1 = valider_choix("012")
                     case _:
-                        print("Veuillez saisir un choix correct")
+                        #print("Veuillez saisir un choix correct")
+                        """print(choice)
+                        cpt1 +=1
+                        #print("Le code a saisir doit etre #100# ou #155#")
+                        test = 0
+                        if cpt1 == 3:
+                            try:
+                                sys.exit()
+                            except SystemExit as e:
+                                print("Vous avez atteint 3 tentives merci de revenir une prochaine fois")
+                        """
                 print(" ")            
                 #choice = valider_choix()
             case _:
-                cpt +=1
+                cpt1 +=1
                 print("Le code a saisir doit etre #100# ou #155#")
                 test = 0
-                if cpt == 3:
+                if cpt1 == 3:
                     try:
                         sys.exit()
                     except SystemExit as e:
@@ -97,5 +211,9 @@ def saisir_code():
 
 def affiche_match155():
     pass        
+
+def compter():
+    pass
+    #cpt = 1
 
 saisir_code()
